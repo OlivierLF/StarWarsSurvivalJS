8516d3182709e3fd1c52730d88f6a0245e23c9f7
OlivierLF <olivier.lefloch96@gmail.com>
2018-01-05 14:52:59 +0100

    add Vador popUp and deleteTrooper when trooper out of canvas

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 var canvas = document.getElementById("cv");
 var context = canvas.getContext("2d");
-var context2 = canvas.getContext("2d");
 
 
 // affiche la glace (le sol) sur tout le canvas
@@ -19,6 +18,19 @@ var drawIce = function () {
 var drawTrooper = function(trooper){
 	context.drawImage(trooper["imTrooper"],trooper["sx"],0,156,180,trooper["x"],trooper["y"]+20,50,50);
 };
+//affiche tous les troopers de la liste entrée en paramètre
+var drawTroopers = function(troopers,vador){
+	troopers.forEach(function(trooper){
+		drawTrooper(trooper);
+		drawVador(vador);
+	});
+}
+
+//affiche Vador passé en paramètre aux coordonnées qui lui sont propres 
+//lors de sa déclaration
+var drawVador = function(vador){
+	context.drawImage(vador["imVador"],vador["sx"],vador["sy"],156,140,vador["x"],vador["y"]+20,70,70);
+};
 
 //affiche le tieFighter associé au trooper passé en paramètre aux coordonnées
 // qui lui sont propres lors de sa déclaration
@@ -27,19 +39,34 @@ var drawTieFighter = function(trooper){
 	tie.src="tieFighter.png";
 	context.drawImage(tie,0,0,810,984,trooper["tieFighterX"],trooper["tieFighterY"],50,50);
 };
+//affiche le tieFighter associé au trooper passé en paramètre aux coordonnées
+// qui lui sont propres lors de sa déclaration
+var drawDestroyer = function(vador){
+	var destroyer = new Image();
+	destroyer.src="destroyer.png";
+	context.drawImage(destroyer,0,0,640,453,vador["destroyerX"],vador["destroyerY"]-20,70,70);
+};
 
 
-//fait appraître les tieFighters (immobiles). Au bout de deux 
-//secondes les tieFighters disparaissent et les troopers 
-//apparaissent et avancent.
-var popUpTroopers = function(troopers){
+//fait appraître les tieFighters (immobiles). Au bout d'une 
+//seconde le tieFighter disparait et le trooper avance.
+var popUp = function(troopers){
+	//popUpVador
+	var xVador=Math.floor(Math.random() * 551);
+	var yVador=Math.floor(Math.random() * 101);
+	var vador=new Vador(xVador,yVador);
+	drawDestroyer(vador);
+	drawVador(vador);
+
+	//popUpTroopers
 	setInterval(function(){
 			var x=Math.floor(Math.random() * 551);
 			var y=Math.floor(Math.random() * 101);
 			var troop=new Trooper(x,y);
 			troopers.push(troop);
-			//drawTieFighter(troop);
+			console.log(troopers.length);
 			drawTrooper(troop);
+			//drawVador(vador);
 			setTimeout(function(){
 				context.drawImage(ice,0,0,64,64,troop["x"],troop["y"],50,50);
 				drawTrooper(troop);
@@ -47,21 +74,27 @@ var popUpTroopers = function(troopers){
 	},2000);
 	setTimeout(function(){
 		setInterval(function(){
-		troopers.forEach(function(trooper){
-			trooper["y"]=trooper["y"]+5;
+			vador["y"]=vador["y"]+1;
 			context.clearRect(0,0,600,800);
-			drawIce();
-			drawTieFighter(trooper);
-			for (var i=0;i<troopers.length;i++){
-				drawTrooper(troopers[i]);
-			}
-			animationTrooper(trooper);
+			drawDestroyer(vador);
+			//drawVador(vador);
+			//animationVador(vador);
+			troopers.forEach(function(trooper){
+				trooper["y"]=trooper["y"]+5;
+				context.clearRect(0,0,600,800);
+				drawIce();
+				drawTieFighter(trooper);
+				animationTrooper(trooper);
+				//drawDestroyer(vador);
+				//drawVador(vador);
+				animationVador(vador);
+				drawTroopers(troopers,vador);
 			});
 		},100);
 	},2000);
+
 	return troopers;
 }
-
 
 //gère le choix de l'image du trooper pour faire l'animation
 function animationTrooper(trooper){
@@ -70,11 +103,37 @@ function animationTrooper(trooper){
 	}
 	trooper["sx"]+=156;
 }
+//gère le choix de l'image de Vador pour faire l'animation
+function animationVador(vador){
+	if (vador["sx"]==600){
+		vador["sx"]=-150;
+	}
+	vador.sx+=150;
+	// le code en dessous fait marcher Vador plus doucement
+	//mais fait clignoter Vador de temps en temps
+	/*
+	if (vador["frame"]%10==0){
+		vador["sx"]+=150;
+	}
+	vador["frame"]++;
+	*/
+	
+}
+
+function deleteTrooper(troopers){
+	setInterval(function(){
+		troopers.forEach(function(trooper){
+				if (trooper.y>800){
+					troopers.splice(troopers[troopers.indexOf(trooper)],1);
+				}
+			});
+	},2500);
+	return troopers;
+}
+	
 
 
-
-
-//Constructeur de l'objet trooper. Ses paramètres (x,y) sont les 
+//Constructeur de l'objet Trooper. Ses paramètres (x,y) sont les 
 //coordonnées de son coin supérieur gauche. sx permet de gérer l'animation
 function Trooper (x, y){
 	this.imTrooper=new Image();
@@ -86,6 +145,22 @@ function Trooper (x, y){
 	this.tieFighterY=y;
 	this.hp=1;
 }
+
+//Constructeur de l'objet Vador. Ses paramètres (x,y) sont les 
+//coordonnées de son coin supérieur gauche. sx permet de gérer l'animation
+function Vador (x, y){
+	this.imVador=new Image();
+	this["imVador"].src="vador.png";
+	this.x=x;
+	this.y=y;
+	this.sx=0;
+	this.sy=300;
+	this.destroyerX=x;
+	this.destroyerY=y;
+	this.hp=25;
+	this.frame=0;
+}
+
 
 
 
@@ -156,28 +231,34 @@ document.addEventListener('mousemove', function viser(position){
 //déclaration de l'image du sol
 var ice=new Image();
 ice.src="ice.jpg";
-
 //déclaration du sprite du trooper
 var trooper=new Image();
 trooper.src="trooper.png";
-
 //déclaration de l'image du tieFighter
 var tie = new Image();
 tie.src="tieFighter.png";
+//déclaration du sprite du trooper
+var vador=new Image();
+vador.src="vador.png";
+//déclaration de l'image du tieFighter
+var destroyer = new Image();
+destroyer.src="destroyer.png";
 
-//démarrage du jeu après 0,1s pour prendre le temps de charger les images
+//démarrage du jeu après 0,5s pour prendre le temps de charger les images
 setTimeout(function(){
 	startGame();
-	}, 100);
+	}, 500);
 
 //Fonction principale du jeu
 function startGame(){
 	drawIce();
 	//déclaration d'une array qui va contenir tous les troopers avec
 	//leur tieFighter
-	troopList=[]
+	troopList=[];
 	//popUp de la liste des troopers ci dessus.
-	troopList=popUpTroopers(troopList);
+	troopList=popUp(troopList);
+	//vadorList=popUpVador(vadorList);
+	troopList=deleteTrooper(troopList);
 
 	
 
